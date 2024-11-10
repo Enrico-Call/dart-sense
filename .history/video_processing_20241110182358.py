1674cfcb99f0c1a9e13f5857c08996bd84ba879f
bnww <ben.willshaw@gmail.com>
2024-11-10 19:47:52 +0000

    Updated README.md with images, description and vid

--- .history/video_processing_20241110182358.py
+++ .history/video_processing_20241110182358.py
@@ -155,8 +155,4 @@ class VideoProcessing:
 
 if __name__ == "__main__":
     pass
-    #game = GameLogic(ruleset='x01', player_names=['Ben'], x01=1001, num_legs=1)
-    #AI_scorer = VideoDetection()
-    #AI_scorer.start("192.168.0.68:8080", game, np.array((1200, 1600)))
-    #AI_scorer.start("external", game)
-    #AI_scorer.start("webcam", game)
+    ~
